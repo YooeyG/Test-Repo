de5e32cc028b4c64b575bc12d4dc3f7684e14c58
Ryan Murphy <Ryan.Murphy12@gmail.com>
2021-08-28 13:15:46 -0400

    first commit

---
+++ Systems.py
@@ -0,0 +1,20 @@
+#Prints out location and whihc python interpreter currently using
+
+import sys
+import os
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet('Ryan'))
+
+print('Version of Python')
+print(sys.version)
+
+print('Virtual Environment')
+print(sys.executable)
+
+print("current directory")
+print(os.getcwd())
